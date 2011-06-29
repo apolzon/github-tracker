5ca8c765c595dd791fc30cc0dcddbbfaee9ff292
Chris Apolzon <chrisa@manilla.com>
2011-06-28 22:25:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubTracker::Application.config.secret_token = '702dd020eee8a9e06816b11cd7d9324d26108a1dec246465a00d8a2cb7610e661bc77edef53c03e1b17d55beaad9f93b7e60dc0f27f288a3f2ad4d0a9e2bab23'
